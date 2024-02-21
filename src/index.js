7fd0479850705d7ed54ab774847807c07c41ab76
Jacob Haap <git@j3.cx>
2024-02-21 21:40:20 +0100

    json method for dcDNS

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 const { getValueFromRecord } = require('./resolve');
+const { getJsonValueFromRecord } = require('./jsonResolve');
 
 const dcDNS = {
     resolve: function(domain) {
         return getValueFromRecord(domain);
+    },
+    jsonResolve: function(domain) {
+        return getJsonValueFromRecord(domain);
     }
 };
 
---
+++ src/jsonResolve.js
@@ -0,0 +1,42 @@
+const fetchDNSTxtRecord = require('./dnsLookup');
+
+async function checkDNSTxtRecord(domain) {
+    const txtRecords = await fetchDNSTxtRecord(domain);
+    return parseTxtRecord(txtRecords);
+}
+
+function parseTxtRecord(txtRecord) {
+    const parts = txtRecord.split(';').map(part => part.trim());
+    const dcPart = parts.find(part => part.startsWith('dc='));
+    if (!dcPart) return null;
+
+    const dcValue = dcPart.substring(3);
+    let result = {
+        type: `_dcdns.${dcValue}`
+    };
+
+    const addrOrContParts = parts.filter(part => part.startsWith('addr=') || part.startsWith('cont='));
+    if (addrOrContParts.length > 0) {
+        if (dcValue === 'hybrid') {
+            result.contents = addrOrContParts.map(part => {
+                const [, protocol, content] = part.split(/\/+/);
+                return { 'content-type': protocol, 'content': content };
+            });
+        } else {
+            const [, protocol, content] = addrOrContParts[0].split(/\/+/);
+            if (protocol && content) {
+                result['content-type'] = protocol;
+                result['content'] = content;
+            }
+        }
+    }
+
+    return result;
+}
+
+exports.getJsonValueFromRecord = async (domain) => {
+    const parsedRecord = await checkDNSTxtRecord(domain);
+    if (!parsedRecord) return JSON.stringify({ error: 'Record not found or could not be parsed' });
+
+    return JSON.stringify(parsedRecord, null, 2);
+};
